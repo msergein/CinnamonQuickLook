fe70b1191b59580a75b66c204b8af3032cdfeb36
Sergey M <msergein@gmail.com>
2022-06-26 13:10:41 +0300

    fix: line break

--- cinnamon_quicklook/__init__.py
+++ cinnamon_quicklook/__init__.py
@@ -5,5 +5,5 @@ class CinnamonQuickLook(DirectoryPaneCommand):
 	def __call__(self):
 		file_under_cursor = self.pane.get_file_under_cursor()
 		if file_under_cursor:
-			os.system("dbus-send --print-reply --dest={dbusDest} {dbusPath} {dbusDest}.ShowFile string:\"{fileName}\" int32:0 boolean:false"
+			os.system("dbus-send --print-reply --dest={dbusDest} {dbusPath} {dbusDest}.ShowFile string:\"{fileName}\" int32:0 boolean:false" \
 				.format(fileName = file_under_cursor, dbusDest="org.nemo.Preview", dbusPath = "/org/nemo/Preview"))
